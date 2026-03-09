b1a6f8e8b2f99155a9ecaffd6d9b6724fc8b4520
HCABurns <harrycaburns@gmail.com>
2026-03-09 17:32:38 +0000

    DBSchema and Tables.
    Adding the DB Scheme for Wallet, Transaction and also a required date_id in a DateCommitments table.

--- wallet/build.gradle.kts
+++ wallet/build.gradle.kts
@@ -1,4 +1,21 @@
+import org.gradle.kotlin.dsl.project
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+dependencies {
+    // Internal modules
+    implementation(project(":common"))
+    implementation(project(":database"))
+    implementation(project(":users"))
+    implementation(project(":dates"))
+
+    // DB Dependencies
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation(libs.postgresql)
 }
