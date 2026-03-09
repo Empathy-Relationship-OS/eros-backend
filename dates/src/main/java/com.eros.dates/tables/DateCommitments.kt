fd2d38c3c08ea1675e01cb34c384f8ca9e8a0dc1
HCABurns <harrycaburns@gmail.com>
2026-03-09 21:53:35 +0000

    Adding integration tests to the repos

--- dates/src/main/java/com.eros.dates/tables/DateCommitments.kt
+++ dates/src/main/java/com.eros.dates/tables/DateCommitments.kt
@@ -2,9 +2,11 @@ package com.eros.dates.tables
 
 import org.jetbrains.exposed.v1.core.Table
 
-object DateCommitments : Table("questions"){
+object DateCommitments : Table("date_commitments"){
 
     // Primary key
     val dateId = long("date_id").autoIncrement()
+
+    override val primaryKey = PrimaryKey(dateId)
 
 }
